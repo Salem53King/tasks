d3e931de4ae15d7a2ec0942f253482e905636184
Jered BOUTE <jeredboute@gmail.com>
2026-08-12 13:57:26 +0100

    Formatage de cli_test

--- test/cli_test.dart
+++ test/cli_test.dart
@@ -21,10 +21,13 @@ void main() {
   });
 
   Future<ProcessResult> runCli(List<String> args) {
-    return Process.run(
-      Platform.resolvedExecutable,
-      ['run', 'bin/tasks.dart', ...args, '--file', filePath],
-    );
+    return Process.run(Platform.resolvedExecutable, [
+      'run',
+      'bin/tasks.dart',
+      ...args,
+      '--file',
+      filePath,
+    ]);
   }
 
   test('La CLI ajoute, complète et supprime une tâche', () async {
@@ -32,7 +35,8 @@ void main() {
     final addResult = await runCli(['add', 'CLI test', 'high']);
     expect(addResult.exitCode, 0);
 
-    final data = jsonDecode(await File(filePath).readAsString()) as List<dynamic>;
+    final data =
+        jsonDecode(await File(filePath).readAsString()) as List<dynamic>;
     expect(data, hasLength(1));
     final id = (data.single as Map<String, dynamic>)['id'] as String;
 
@@ -42,10 +46,7 @@ void main() {
 
     final completedData =
         jsonDecode(await File(filePath).readAsString()) as List<dynamic>;
-    expect(
-      (completedData.single as Map<String, dynamic>)['isCompleted'],
-      true,
-    );
+    expect((completedData.single as Map<String, dynamic>)['isCompleted'], true);
 
     // Supprimer la tâche
     final deleteResult = await runCli(['delete', id]);
